edfbaf3f6007312572752b3154b40bbda4cb362d
Jaron <jayisaac6706@gmail.com>
2026-05-15 19:57:22 -0700

    removeAt method made, with edge cases implemented:

--- src/Practice.java
+++ src/Practice.java
@@ -3,12 +3,15 @@ public class Practice {
         Node myNode = new Node('w', null);
         Node otherNode = new Node('x', null);
         Node coolNode = new Node('e', null);
-
+        Node helloNode = new Node('f', null);
         myNode.next = otherNode;
         otherNode.next = coolNode;
+        coolNode.next = helloNode;
 
-        int numNodes = myNode.length();
-        System.out.println(numNodes);
+        //int numNodes = myNode.length();
+        //System.out.println(numNodes);
+
+        Node newHead = removeAt(myNode, 2);
 
         Node current = myNode;
 
@@ -17,5 +20,31 @@ public class Practice {
             current = current.next;
         }
 
+    }
+
+    // removes node at removeIdx
+    // head is at index
+    // example w->x->e->f
+    // removeIdx 2
+    // result w->x->f
+    // Return the new head of the list
+    public static Node removeAt(Node head, int removeIdx){
+        if(removeIdx < 0){
+            throw new IndexOutOfBoundsException("No sir can do. removeIdx must be greater/equal >= 0.");
+        }
+
+        if(removeIdx == 0){
+            return head.next;
+        }
+        Node current = head;
+
+        for(int i = 0; i < removeIdx - 1; i++){
+            if(current == null){
+                throw new IndexOutOfBoundsException("Index out of bounds. removeIdx must be less than length of list.");
+            current = current.next;
+        }
+        current.next = current.next.next;
+
+        return head;
     }
 }
